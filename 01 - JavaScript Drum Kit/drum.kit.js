e9a776085d9231322f5da4a514b2678469382688
Cory Kim <corykim0829@gmail.com>
2019-02-27 19:29:48 +0900

    feat: play sounds and remove transition

---
+++ 01 - JavaScript Drum Kit/drum.kit.js
@@ -0,0 +1,19 @@
+function playSound(e){
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if(!audio) return; // stop the function from running all together
+	audio.currentTime = 0; // rewind to the start
+	audio.play();
+	key.classList.add('playing');
+}
+
+function removeTransition(e) {
+	 if(e.propertyName != 'transform') return; // skip it if it's not a transform
+	 this.classList.remove('playing');
+}
+
+function setDrumKit(){ // complie error, need to make a function for it
+	window.addEventListener('keydown', playSound);
+	const keys = document.querySelectorAll('.key');
+	keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+}
