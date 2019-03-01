d37ceddb397aade862b2bac428823aa6bae76394
Cory Kim <corykim0829@gmail.com>
2019-03-01 15:45:13 +0900

    refactor: functions

--- 01 - JavaScript Drum Kit/drum.kit.js
+++ 01 - JavaScript Drum Kit/drum.kit.js
@@ -1,9 +1,13 @@
 function playSound(e){
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 	if(!audio) return; // stop the function from running all together
 	audio.currentTime = 0; // rewind to the start
 	audio.play();
+	animateKey(e);
+}
+
+function animateKey(e){
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 	key.classList.add('playing');
 }
 
@@ -14,6 +18,10 @@ function removeTransition(e) {
 
 function setDrumKit(){ // complie error, need to make a function for it
 	window.addEventListener('keydown', playSound);
+	addKeyEventListener();
+}
+
+function addKeyEventListener() {
 	const keys = document.querySelectorAll('.key');
 	keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 }
