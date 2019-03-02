1ca7f5c312365b9ba00537fb553bbe082b5bf404
Jung Hoe Hyeong <commaniakr@gmail.com>
2019-03-02 21:09:11 +0900

    refactor: 함수명 등 수정 사항 반영.

--- 01 - JavaScript Drum Kit/drum.js
+++ 01 - JavaScript Drum Kit/drum.js
@@ -5,24 +5,31 @@ function getKey(e) {
     return document.querySelector(`.key[data-key="${e.keyCode}"]`);
 }
 
-function initiateDrum(){
+function initiateDrum() {
     var keys = document.querySelectorAll('.key');
     window.addEventListener('keydown', playDrum);
-    keys.forEach(key => key.addEventListener('transitionend', endTransition));
+    keys.forEach(key => key.addEventListener('transitionend', returnKeys));
 }
 
-function playDrum(e){
-    if(!getAudio(e)){
-        return -1;
+function playDrum(e) {
+    if(!getKey(e)) {
+        return false;
     }
     getKey(e).classList.add('playing');
+    playAudio(e);
+}
+
+function playAudio(e) {
+    if(!getAudio(e)) {
+        return false;
+    }
     getAudio(e).currentTime = 0; // 재생 시간 처음으로 돌리기
     getAudio(e).play();
 }
 
-function endTransition(e){
+function returnKeys(e) {
     if(e.propertyName != 'transform') {
-        return -1;
-    } 
+        return false;
+    }
     this.classList.remove('playing');
 }
