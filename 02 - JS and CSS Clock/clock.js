63e48716e7fdfb552babf40b628295bdd994d83f
unknown <a01054394111@gmail.com>
2019-03-07 15:57:57 +0900

    fix : degrees in js, html (각도 조절)

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -5,11 +5,11 @@ function $(selector) {
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360)
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
     const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes / 60) * 360)
+    const minutesDegrees = ((minutes / 60) * 360)+ 90;
     const hours = now.getHours();
-    const hoursDegrees = ((hours / 12) * 360)
+    const hoursDegrees = ((hours / 12) * 360) + 90;
 
     $(".second-hand").style.transform = `rotate(${secondsDegrees}deg)`;
     $(".min-hand").style.transform = `rotate(${minutesDegrees}deg)`;
