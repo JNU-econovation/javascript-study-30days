5c8e9f64ae21088e770ce6e641cc63fd8a63a46f
Jung Hoe Hyeong <commaniakr@gmail.com>
2019-03-07 21:11:25 +0900

    add: 실시간 반영되지 않았던 문제 수정, 동영상 가이드를 참고하여 완성

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -1,7 +1,19 @@
+function $(component) {
+    return document.querySelector(component);
+}
+
 function setDate() {
     const now = new Date();
-    const seconds =  now.getSeconds();
-    console.log(seconds);
 
-    setInterval(setDate, 1000);
+    const second = now.getSeconds();
+    const secondDegree = ((second / 60) * 360) + 90;
+    $(".second-hand").style.transform = `rotate(${secondDegree}deg)`;
+    
+    const minute = now.getMinutes();
+    const minuteDegree = ((minute / 60) * 360) + 90;
+    $(".min-hand").style.transform = `rotate(${minuteDegree}deg)`;
+
+    const hour = now.getHours();
+    const hourDegree = ((hour / 12) * 360) + ((minute / 60) * 360)/12  + 90;
+    $(".hour-hand").style.transform = `rotate(${hourDegree}deg)`;
 }
