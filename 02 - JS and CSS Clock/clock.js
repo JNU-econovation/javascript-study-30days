aac1bb42aebdd6e71492ea882322a9c7a4af6b9d
unknown <a01054394111@gmail.com>
2019-03-07 15:50:29 +0900

    feat : setDate in js

---
+++ 02 - JS and CSS Clock/clock.js
@@ -0,0 +1,17 @@
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360)
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360)
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360)
+
+    $(".second-hand").style.transform = `rotate(${secondsDegrees}deg)`;
+    $(".minute-hand").style.transform = `rotate(${minutesDegrees}deg)`;
+    $(".hour-hand").style.transform = `rotate(${hoursDegrees}deg)`;
+}
