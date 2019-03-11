4381bf93f055cc55db60547bc7f0964bb9706213
mywnajsldkf <mywnajsldkf@gmail.com>
2019-03-11 21:21:13 +0900

    complete DAY2

--- 02 - JS and CSS Clock/START.js
+++ 02 - JS and CSS Clock/START.js
@@ -3,29 +3,29 @@ const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 const now = new Date();
 
-  function setDate() {
-    setSeconds();
-    setMinutes();
-    setHours();
-  }
+function setDate() {
+  setSeconds();
+  setMinutes();
+  setHours();
+}
 
-  function setSeconds() {
-    const now = new Date();
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  }
+function setSeconds() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+}
 
-  function setMinutes() {
-    const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + 90;
-    minsHand.style.transform = `rotate(${minsDegrees}deg)`
-  }
+function setMinutes() {
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`
+}
 
-  function setHours() {
-    const hour = now.getHours();
-    const hourDegrees = ((hour / 60) * 360) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-  }
+function setHours() {
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 60) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
 
-  setInterval(setDate, 1000)
+setInterval(setDate, 1000)
