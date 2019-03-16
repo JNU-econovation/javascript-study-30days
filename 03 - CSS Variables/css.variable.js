e85dbd405571be3183f404fd163a314cd54c5120
corykim0829 <corykim0829@gmail.com>
2019-03-16 16:39:20 +0900

    feat: set property in handleUpdate function

--- 03 - CSS Variables/css.variable.js
+++ 03 - CSS Variables/css.variable.js
@@ -2,10 +2,10 @@
 window.onload = function() {
     const inputs = document.querySelectorAll('.controls input');
     inputs.forEach(input => input.addEventListener('change', handleUpdate));    
+    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));    
 };
 
 function handleUpdate() {
-    console.log(this.value);
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
-
-
