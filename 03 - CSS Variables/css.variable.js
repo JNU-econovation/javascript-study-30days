4e66be98130edb5077fc362cee6710a89deb4620
corykim0829 <corykim0829@gmail.com>
2019-03-17 19:02:57 +0900

    fix: semicolon, suffix

--- 03 - CSS Variables/css.variable.js
+++ 03 - CSS Variables/css.variable.js
@@ -3,9 +3,9 @@ window.onload = function() {
     const inputs = document.querySelectorAll('.controls input');
     inputs.forEach(input => input.addEventListener('change', handleUpdate));    
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));    
-};
+}
 
 function handleUpdate() {
-    const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    const unit = this.dataset.unit || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + unit);
 }
