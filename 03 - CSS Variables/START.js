ec566695921d1377dfa8f930a8c397444d23109a
mywnajsldkf <mywnajsldkf@gmail.com>
2019-03-19 15:29:36 +0900

    Day3 refactoring

--- 03 - CSS Variables/START.js
+++ 03 - CSS Variables/START.js
@@ -1,8 +1,8 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
- const picSize = this.dataset.sizing || '';
- document.documentElement.style.setProperty(`--${this.name}`, this.value + picSize);
+ const picMeasure = this.dataset.sizing || '';
+ document.documentElement.style.setProperty(`--${this.name}`, this.value + picMeasure);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
