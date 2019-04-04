7b60a32d4ecde3c16fcb390bef47abc390f22a3d
Nina <seoinha4964@gmail.com>
2019-04-04 19:51:46 +0900

    js30 review

--- 01 - JavaScript Drum Kit/js30.js
+++ 01 - JavaScript Drum Kit/js30.js
@@ -1,20 +1,28 @@
-window.addEventListener('keydown', playSound);
-
+function addKeydown(){
+    window.addEventListener('keydown', playSound);
+}
 function playSound(e) { 
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`); 
     
     if (!audio) return; 
-    key.classList.add('playing'); 
+    audio.currentTime = 0;
     audio.play(); 
+    remo
 }
-
-audio.currentTime = 0;
-
+function stopSound(){
+    const keys = Array.from(document.querySelectorAll('.key'));
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+}
+function remotePlaying(key){
+    key.classList.add('playing'); 
+}
 function removeTransition(e) { 
     if (e.propertyName !== 'transform') return; 
     e.target.classList.remove('playing'); 
+    stopSound();
 }
-
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+function init(){
+    stopSound();
+    addKeydown();
+}
